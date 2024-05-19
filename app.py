f8c815abfe836e92c39a96f3449cbca08a4b72ff
LucianoBorba <luhborbafilho@gmail.com>
2024-05-19 12:28:54 -0300

    feat: criando munu de funções

--- app.py
+++ app.py
@@ -2,38 +2,36 @@
 
 import streamlit as st
 
+from src.create_cliente import criar_cliente
+
 
 def main():
     """Função principal."""
     st.set_page_config(page_title="Sistema Gerencial", layout="wide")
     st.title("Sistema Gerencial")
 
-    with st.form("cadastrocliente"):
-        col1, col2, col3 = st.columns(3)
-        with col1:
-            nome = st.text_input("Nome Completo")
-            email = st.text_input("Email")
-            endereco_rua = st.text_input("Logradouro")
-            endereco_bairro = st.text_input("Bairro")
-        with col2:
-            dt_nascimento = st.date_input("Data de Nascimento", value=None)
-            cpf = st.text_input("CPF")
-            endereco_numero = st.number_input("Nº", value=0, step=1)
-            endereco_cidade = st.text_input("Cidade")
-        with col3:
-            sexo = st.selectbox(
-                "Sexo", ["Masculino", "Feminino"], placeholder="", index=None
-            )
-            estado_civil = st.selectbox(
-                "Estado Civil",
-                ["Solteiro(a)", "Casado(a)", "Viuvo(a)", "Divorciado(a)"],
-                placeholder="",
-                index=None,
-            )
-            endereco_complemento = st.text_input("Complemento")
-            endereco_uf = st.text_input("UF")
+    st.title("Página de Clientes")
+    tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs(
+        ["Home", "Cadastrar", "Listar", "Editar", "Excluir", "Relatórios"]
+    )
+    with tab1:
+        st.header("Home")
 
-        cadastrar = st.form_submit_button()
+    with tab2:
+        st.header("Cadastrar")
+        criar_cliente()
+
+    with tab3:
+        st.header("Listar")
+
+    with tab4:
+        st.header("Editar")
+
+    with tab5:
+        st.header("Excluir")
+
+    with tab6:
+        st.header("Relatórios")
 
 
 if __name__ == "__main__":
